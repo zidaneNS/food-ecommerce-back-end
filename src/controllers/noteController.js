a540f2f555dbec10ef0fb55b116a68bd1b5251c0
unknown <zidane.nur004@gmail.com>
2024-11-29 21:07:41 +0700

    commit prefinal

--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -4,6 +4,7 @@ const { format } = require('date-fns');
 const { v4: uuid } = require('uuid');
 const { Enkripsi_shift_chipher, Deskripsi_shift_chipher, generateShiftKey } = require('./shift_chipher')
 const { Enkripsi_vigenere, generateVigenereKey, Deskripsi_vigenere } = require('./vigenere')
+const { Enkripsi_des, generateDesKey, Deskripsi_des } = require('./des')
 
 const getAllNotes = async (req, res) => {
     try {
@@ -31,13 +32,15 @@ const addNote = async (req, res) => {
 
         const shift_key = generateShiftKey();
         const viginere_key = generateVigenereKey();
-        const des_key = uuid();
+        const des_key = uuid(); // kalo udah ada fungsinya baris ini dikomen aja
+        // const des_key = generateDesKey(); //ini diuncomment kalo udah jadi  algoritmanya
 
         console.log(shift_key);
 
-        const shift_text = Enkripsi_shift_chipher(dataInput, shift_key)
+        const shift_text = Enkripsi_shift_chipher(dataInput, shift_key);
         const viginere_text = Enkripsi_vigenere(dataInput, viginere_key);
-        const des_text = jwt.sign(data, des_key);
+        const des_text = jwt.sign(data, des_key); //ini komen aja kalo udah selesai
+        // const des_text = Enkripsi_des(dataInput, des_key);
 
         const result = await Note.create({
             shift_text,
